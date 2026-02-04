cab88477e3e4e26a797d2b387b14bdcef5d690d6
ritikvarun <hs0037118@gmail.com>
2026-02-04 11:10:58 +0530

    Fix import path case sensitivity for AuthContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AuthContext from './context/authContext.jsx'
+import AuthContext from './context/AuthContext.jsx'
 import UserContext from './context/UserContext.jsx'
 import ShopContext from './context/ShopContext.jsx'
 
