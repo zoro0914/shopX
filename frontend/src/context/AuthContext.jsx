f0c1a72ae61ea8892bde37cf1f16dbbbe8227a20
ritikvarun <hs0037118@gmail.com>
2026-02-04 11:07:09 +0530

    Update frontend to use deployed backend URL

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "https://shopx-1-z0x4.onrender.com"
 
     let value = {
        serverUrl
     }
   return (
-
     
     <div>
         <authDataContext.Provider value={value}>
