4540ccc96d1d895eb680883e490e6aac9f8ae130
ritikvarun <hs0037118@gmail.com>
2026-02-04 11:42:50 +0530

    Update CORS configuration for admin panel

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(cors({
  origin: process.env.NODE_ENV === 'production' 
    ? [process.env.FRONTEND_URL] 
-   : ["http://localhost:5173", "http://localhost:5174"],
+   : ["https://shopx-2.onrender.com", "http://localhost:5174"],
  credentials:true
 }))
 
